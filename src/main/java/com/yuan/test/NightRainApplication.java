799222406f5893127e709c00fa3d796b1d0df3dd
yuanfacheng <yuanfacheng7032@163.com>
2023-02-19 14:20:11 +0800

    初始化

---
+++ src/main/java/com/yuan/test/NightRainApplication.java
@@ -0,0 +1,11 @@
+package com.yuan.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightRainApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NightRainApplication.class, args);
+	}
+}
---
+++ src/main/java/com/yuan/test/TestController.java
@@ -0,0 +1,23 @@
+package com.yuan.test;
+
+import javax.annotation.Resource;
+
+import com.yuan.autoconfig.HelloService;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestController implements CommandLineRunner {
+ 
+    /**
+     * 注入自定义starter服务
+     */
+    @Resource
+    private HelloService helloService;
+ 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(helloService.hello());
+    }
+}
