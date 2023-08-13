3ebe9ab581286c6ea8d6c8963f09af215a1e62bf
yuanfacheng <yuanfacheng7032@163.com>
2023-08-13 14:34:27 +0800

    2222

--- Main7001/src/main/java/com/yuan/test/NightRainApplication.java
+++ Main7001/src/main/java/com/yuan/test/NightRainApplication.java
@@ -1,5 +1,7 @@
 package com.yuan.test;
 
+import com.sun.xml.bind.v2.runtime.output.SAXOutput;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +11,6 @@ public class NightRainApplication {
 	public static void main(String[] args) {
 		//https://www.bookstack.cn/read/spring-cloud-docs/README.md
 		SpringApplication.run(NightRainApplication.class, args);
+		System.out.println("1111111");
 	}
 }
